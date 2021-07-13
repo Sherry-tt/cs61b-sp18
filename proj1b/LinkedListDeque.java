cef2f2f01840e1d1df45760b849bd1680a3cd42c
rtan <793346994@qq.com>
2021-07-13 10:56:59 +0800

    proj1b V2.0

--- proj1b/LinkedListDeque.java
+++ proj1b/LinkedListDeque.java
@@ -6,17 +6,17 @@ public class LinkedListDeque<Item> implements Deque<Item>  {
         private LinkedNode prev;
         private LinkedNode next;
 
-        public LinkedNode(Item i, LinkedNode p, LinkedNode n){
+        LinkedNode(Item i, LinkedNode p, LinkedNode n) {
             item = i;
             prev = p;
             next = n;
         }
 
-        private Item getRecursive(int index){
-            if(index == 0){
+        private Item getRecursive(int index) {
+            if (index == 0) {
                 return this.item;
             }
-            return next.getRecursive(index -1);
+            return next.getRecursive(index - 1);
         }
     }
 
@@ -24,7 +24,7 @@ public class LinkedListDeque<Item> implements Deque<Item>  {
     private LinkedNode last;
     private int size;
 
-    public LinkedListDeque(){
+    public LinkedListDeque() {
         sentinel = new LinkedNode(null, null, null);
         sentinel.prev = sentinel;
         sentinel.next = sentinel;
@@ -32,26 +32,26 @@ public class LinkedListDeque<Item> implements Deque<Item>  {
         size = 0;
     }
 
-//	public LinkedListDeque(T i){
-//		sentinel = new LinkedNode(null, null, null);
-//		sentinel.next = new LinkedNode(i, sentinel, sentinel);
-//		sentinel.prev = sentinel.next;
-//		last = sentinel.next;
-//		size = 1;
-//	}
+//      public LinkedListDeque(T i){
+//      sentinel = new LinkedNode(null, null, null);
+//      sentinel.next = new LinkedNode(i, sentinel, sentinel);
+//      sentinel.prev = sentinel.next;
+//      last = sentinel.next;
+//      size = 1;
+// }
     @Override
-    public void addFirst(Item x){
+    public void addFirst(Item x) {
         sentinel.next = new LinkedNode(x, sentinel, sentinel.next);
         sentinel.next.next.prev = sentinel.next;
         // trace last when only one item in the list and then removed
-        if(size == 0) {
+        if (size == 0) {
             last = sentinel.next;
         }
         size += 1;
     }
 
     @Override
-    public void addLast(Item x){
+    public void addLast(Item x) {
         last.next = new LinkedNode(x, last, sentinel);
         last = last.next;
         sentinel.prev = last;
@@ -59,8 +59,8 @@ public class LinkedListDeque<Item> implements Deque<Item>  {
     }
 
     @Override
-    public Item removeFirst(){
-        if(isEmpty()){
+    public Item removeFirst() {
+        if (isEmpty()) {
             System.out.println("empty list!!!");
             return null;
         }
@@ -69,7 +69,7 @@ public class LinkedListDeque<Item> implements Deque<Item>  {
         sentinel.next = sentinel.next.next;
         size -= 1;
         // track last pointer
-        if(size == 0) {
+        if (size == 0) {
             last = sentinel;
             sentinel.prev = sentinel;
             return result;
@@ -79,8 +79,8 @@ public class LinkedListDeque<Item> implements Deque<Item>  {
     }
 
     @Override
-    public Item removeLast(){
-        if(isEmpty()){
+    public Item removeLast() {
+        if (isEmpty()) {
             System.out.println("empty list!!!");
             return null;
         }
@@ -95,35 +95,35 @@ public class LinkedListDeque<Item> implements Deque<Item>  {
 
     /** */
     @Override
-    public Item get(int index){
+    public Item get(int index) {
         LinkedNode p = sentinel.next;
-        for (int i = 0; i < index; i++){
+        for (int i = 0; i < index; i++) {
             p = p.next;
         }
         return p.item;
     }
 
     /** recursive */
-    public Item getRecursive(int index){
-        if(size() == 0 || index >= size()){
+    public Item getRecursive(int index) {
+        if (size() == 0 || index >= size()) {
             return null;
         }
         return sentinel.next.getRecursive(index);
     }
 
-    private Item getFirst(){
+    private Item getFirst() {
         return sentinel.next.item;
     }
 
-    private Item getLast(){
+    private Item getLast() {
         return sentinel.prev.item;
     }
 
     /** */
     @Override
-    public void printDeque(){
+    public void printDeque() {
         LinkedNode p = sentinel.next;
-        for (int i = 0; i < size(); i++){
+        for (int i = 0; i < size(); i++) {
             System.out.println(p.item);
             p = p.next;
         }
@@ -131,15 +131,13 @@ public class LinkedListDeque<Item> implements Deque<Item>  {
 
     @Override
     /** using caching method, oop programming, everything is controlled by object  */
-    public int size(){
+    public int size() {
         return size;
     }
 
     @Override
     /** using caching method, oop programming, everything is controlled by object  */
-    public boolean isEmpty(){
+    public boolean isEmpty() {
         return (size == 0);
     }
-
-
 }
--- proj1b/TestOffByOne.java
+++ proj1b/TestOffByOne.java
@@ -4,12 +4,12 @@ import static org.junit.Assert.*;
 public class TestOffByOne {
     // You must use this CharacterComparator and not instantiate
     // new ones, or the autograder might be upset.
-    //static CharacterComparator offByOne = new OffByOne();
+    static CharacterComparator offByOne = new OffByOne();
 
     // Your tests go here.
     @Test
     public void equalChars() {
-        CharacterComparator offByOne = new OffByOne();
+
         assertTrue(offByOne.equalChars('c', 'x'));
     }
 
